fa2833e5547315e691ace1cccbee693bbea01d7a
Mert Talha Şimşek <merttalhasimsek@gmail.com>
2023-08-04 09:25:43 +0300

    Screenshots Update

--- SnapshotDemo-WindowsForms/Form1.Designer.cs
+++ SnapshotDemo-WindowsForms/Form1.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCapture_Click = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnCapture_Click
+            // 
+            this.btnCapture_Click.Location = new System.Drawing.Point(274, 162);
+            this.btnCapture_Click.Name = "btnCapture_Click";
+            this.btnCapture_Click.Size = new System.Drawing.Size(179, 79);
+            this.btnCapture_Click.TabIndex = 0;
+            this.btnCapture_Click.Text = "Take Screenshots";
+            this.btnCapture_Click.UseVisualStyleBackColor = true;
+            this.btnCapture_Click.Click += new System.EventHandler(this.btnCapture_Click_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCapture_Click);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCapture_Click;
     }
 }
 
--- SnapshotDemo-WindowsForms/Form1.cs
+++ SnapshotDemo-WindowsForms/Form1.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace SnapshotDemo_WindowsForms
 {
     public partial class Form1 : Form
     {
+        private static Bitmap bmpScreenshot;
+        private static Graphics gfxScreenshot;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +21,40 @@ namespace SnapshotDemo_WindowsForms
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnCapture_Click_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Uygulama penceresinin boyutları ve konumunu alın.
+                Rectangle appBounds = this.Bounds;
+
+                // Uygulama penceresinin boyutlarında bir Bitmap nesnesi oluşturun.
+                Bitmap screenshot = new Bitmap(appBounds.Width, appBounds.Height);
+
+                // Graphics nesnesi oluşturarak uygulama penceresinin ekran görüntüsünü yakalayın.
+                using (Graphics graphics = Graphics.FromImage(screenshot))
+                {
+                    graphics.CopyFromScreen(appBounds.Location, new Point(0, 0), appBounds.Size);
+                }
+
+                // Ekran görüntüsünü kaydetmek için dosya yolunu alın (örnek olarak masaüstüne kaydedelim).
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string fileName = "screenshot_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+                string filePath = System.IO.Path.Combine(desktopPath, fileName);
+
+                // Ekran görüntüsünü dosyaya kaydedin.
+                screenshot.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+
+                // Kullanıcıya bildirim verin.
+                MessageBox.Show("Uygulama ekranı başarıyla kaydedildi:\n" + filePath, "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda kullanıcıya uyarı verin.
+                MessageBox.Show("Bir hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
